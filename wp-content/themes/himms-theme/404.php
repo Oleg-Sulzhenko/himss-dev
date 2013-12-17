3b3d51ac6516462b1c8907703eec12003564b5ff
Sulzhenko-Oleg <webaleg@gmail.com>
2013-12-17 17:36:28 +0200

    try ignore2

--- wp-content/themes/himms-theme/404.php
+++ wp-content/themes/himms-theme/404.php
@@ -8,12 +8,15 @@ get_header(); ?>
 
   <div id="main" role="main">
 
+      
     <details>
       <summary><h1>Not found</h1></summary>
       <p><span frown>:(</span></p>
     </details>
+      
 
   </div>
+
 
 <?php get_sidebar(); ?>
 
